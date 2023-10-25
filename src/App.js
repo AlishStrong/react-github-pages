e25b1deb40378105850815c2212dfe8773e62e21
Alisher Aliev <alisher.aliev@tietoevry.com>
2023-10-25 21:48:19 +0300

    Remove default things from Create React App; Add React Router and simple Views;

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import { Link, Route, BrowserRouter, Routes } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ display: 'flex', flexDirection: 'column', width: '50%', margin: 'auto'}}>
+      <BrowserRouter basename="/react-github-pages">
+        <NavLinks />
+        <Routes>
+          <Route path='/' element={<HomePage />}></Route>
+          <Route path='/about' element={<AboutPage />}></Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
+}
+
+function NavLinks() {
+  return (
+    <div style={{ display: 'flex', gap: '20px', margin: '20px 0px'}}>
+      <Link to="/">home</Link>
+      <Link to="/about">about</Link>
+    </div>
+  )
+}
+
+function HomePage() {
+  return (
+    <div>Home View</div>
+  )
+}
+
+function AboutPage() {
+  return (
+    <div>About View</div>
+  )
 }
 
 export default App;
